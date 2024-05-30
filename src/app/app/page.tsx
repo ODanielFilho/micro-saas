92098a2a2496e174d5061d42fcebbaa9356190d3
Daniel Filho <danielfsf2001@gmail.com>
2024-05-30 12:41:35 -0300

    end setp 1

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,12 @@
-"use client";
+import { auth } from "@/services/auth";
+import { UserInfo } from "./_components/user-info";
 
-export default function Page() {
-  return <h1>oi</h1>;
+export default async function Page() {
+  const session = await auth();
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <UserInfo user={session?.user} />
+    </main>
+  );
 }
